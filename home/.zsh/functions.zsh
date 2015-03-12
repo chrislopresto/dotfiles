54f04aac26a86c57e2a6a197cfd73605f534b538
Chris LoPresto <chrislopresto@gmail.com>
2015-03-12 02:03:30 -0400

    Add watchman_reset

--- home/.zsh/functions.zsh
+++ home/.zsh/functions.zsh
@@ -2,3 +2,7 @@
 function check_compression {
   curl -I -H 'Accept-Encoding: gzip,deflate' $1 | grep "Content-Encoding"
 }
+
+function watchman_reset {
+  (for root in $(watchman watch-list | jq '.roots | .[]'); do watchman watch-del $(echo $root | sed 's/"//g') ; done)
+}
