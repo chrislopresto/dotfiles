989569c84eb64a5371db9d45a9c450579b7a935f
Chris LoPresto <chris@chrislopresto.com>
2016-05-18 10:55:02 -0400

    Update tmux config

--- home/.zsh/functions.zsh
+++ home/.zsh/functions.zsh
@@ -31,3 +31,9 @@ function stp {
   fi
   st *.sublime-project
 }
+
+function t() {
+  # Defaults to 3 levels deep, do more with `t 5` or `t 1`
+  # pass additional args after
+  tree -I '.git|node_modules|bower_components|.DS_Store' --dirsfirst --filelimit 150 -L ${1:-3} -aC $2
+}
