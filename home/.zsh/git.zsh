a4b7c5fda2200a2640b08fe1f200ea953f05590a
Chris LoPresto <chris.lopresto@betterment.com>
2016-07-28 13:03:48 -0400

    Add some git aliases

--- home/.zsh/git.zsh
+++ home/.zsh/git.zsh
@@ -2,8 +2,14 @@ alias glt="git log --since=midnight --author='Chris LoPresto' --oneline"
 alias gltc="glt | awk '\$1=\"\" ; sub(/^ /, \"\")'"
 alias gltcs="gltc | awk '{printf \"%s. \",\$0}'"
 alias gdlm="git branch --merged | grep -v '^* master$' | grep -v '^  master$' | xargs git branch -d"
-alias gcmm="gcm ; gl --prune ; gdlm"
+alias gdlmd="git branch --merged | grep -v '^* develop$' | grep -v '^  develop$' | xargs git branch -d"
 alias gundo="git reset --soft HEAD~1"
+alias grid="git rebase --interactive --autosquash develop"
+alias grim="git rebase --interactive --autosquash master"
+alias gfc="gc --fixup"
+alias gcod="gco develop"
+alias gcmm="gcm ; gl --prune ; gdlm"
+alias gcdd="gcod ; gl --prune ; gdlmd"
 
 function demaster {
   if [[ $# -eq 0 ]] ; then
