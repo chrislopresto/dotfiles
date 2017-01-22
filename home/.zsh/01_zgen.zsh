649d013299c40a556836395b8cd2e9a83c91e152
Chris LoPresto <chris@chrislopresto.com>
2017-01-22 03:48:05 -0500

    Finish up a few git, brew, zgen things

--- home/.zsh/01_zgen.zsh
+++ home/.zsh/01_zgen.zsh
@@ -3,10 +3,10 @@ export ZGEN_RESET_ON_CHANGE=($HOME/.zshrc)
 source ~/.zgen/zgen.zsh
 
 if ! zgen saved; then
+  zgen prezto
+  zgen prezto directory
   zgen prezto editor key-bindings 'vi'
   zgen prezto prompt theme 'pure'
-
-  zgen prezto
   zgen prezto git
   zgen prezto command-not-found
   zgen prezto tmux
