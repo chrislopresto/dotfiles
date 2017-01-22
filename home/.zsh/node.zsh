2041a5f229d074894c34ca5acc41eca6246dedd6
Chris LoPresto <chris@chrislopresto.com>
2017-01-22 00:30:05 -0500

    Install node versions

--- home/.zsh/node.zsh
+++ home/.zsh/node.zsh
@@ -1,6 +1,3 @@
-export NODE_ENV=dev
-
-export NVM_DIR="/Users/chrislopresto/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+eval "$(nodenv init -)"
 
 alias lnm='ls -lh node_modules | grep "\->"'
