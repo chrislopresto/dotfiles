5a0e58de97fbf082337309c1b852329829be022d
Chris LoPresto <chris@chrislopresto.com>
2017-01-23 09:30:58 -0500

    Add ellipsis.sh

---
+++ ellipsis.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+pkg.link() {
+  # Directories
+  fs.link_file atom
+  fs.link_file cider
+
+  mkdir -p "$ELLIPSIS_HOME/.config"
+  fs.link_file config/heroku "$ELLIPSIS_HOME/.config/heroku"
+  fs.link_file config/nvim "$ELLIPSIS_HOME/.config/nvim"
+  fs.link_file config/pgcli "$ELLIPSIS_HOME/.config/pgcli"
+
+  fs.link_file tmuxinator
+  fs.link_file vim
+  fs.link_file zsh
+
+  # Files
+  fs.link_file Brewfile
+  fs.link_file gemrc
+  fs.link_file gitconfig
+  fs.link_file gitignore
+  fs.link_file hyper.js
+  fs.link_file node-version
+  fs.link_file pryrc
+  fs.link_file tmux.conf
+  fs.link_file zshrc
+}
+
+pkg.unlink() {
+  rm "$ELLIPSIS_HOME/.config/heroku"
+  rm "$ELLIPSIS_HOME/.config/nvim"
+  rm "$ELLIPSIS_HOME/.config/pgcli"
+
+  hooks.unlink
+}
