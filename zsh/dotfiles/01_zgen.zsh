4212b56ef0ddf414c3988baecc935d15afc49490
Chris LoPresto <chris.lopresto@betterment.com>
2017-02-01 19:19:45 -0500

    Remove prezto editor vi key-bindings

--- zsh/dotfiles/01_zgen.zsh
+++ zsh/dotfiles/01_zgen.zsh
@@ -4,7 +4,6 @@ source ~/.zgen/zgen.zsh
 if ! zgen saved; then
   zgen prezto
   zgen prezto directory
-  zgen prezto editor key-bindings 'vi'
   zgen prezto prompt theme 'pure'
   zgen prezto git
   zgen prezto command-not-found
