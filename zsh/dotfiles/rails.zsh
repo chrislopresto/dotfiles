e9c8743f280938670fa65de3cb97221a37c7befe
Chris LoPresto <chris.lopresto@betterment.com>
2017-02-06 10:28:03 -0500

    Tidy up a few things

--- zsh/dotfiles/rails.zsh
+++ zsh/dotfiles/rails.zsh
@@ -2,3 +2,8 @@ alias rake='noglob bundle exec rake'
 alias beg='bundle exec guard start'
 alias rakeall='time bundle exec rake db:drop db:create db:migrate db:seed db:test:prepare'
 alias rakeallt='time bundle exec rake db:drop --trace db:create --trace db:migrate --trace db:seed --trace db:test:prepare --trace'
+
+function dbrr() {
+  rake db:migrate:down VERSION=$1
+  rake db:migrate:up VERSION=$1
+}
