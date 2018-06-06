1d76c01e5586536cfd2aff27244522a29b37cafc
Chris LoPresto <chris.lopresto@betterment.com>
2018-06-06 03:40:33 -0400

    Restore some aliases

--- zsh/dotfiles/aliases.zsh
+++ zsh/dotfiles/aliases.zsh
@@ -22,3 +22,12 @@ alias st="subl"
 function marked() {
   open $1 -a Marked\ 2
 }
+
+alias cp='nocorrect cp'
+alias ln='nocorrect ln'
+alias mv='nocorrect mv'
+alias rm='nocorrect rm'
+alias cpi="${aliases[cp]:-cp} -i"
+alias lni="${aliases[ln]:-ln} -i"
+alias mvi="${aliases[mv]:-mv} -i"
+alias rmi="${aliases[rm]:-rm} -i"
