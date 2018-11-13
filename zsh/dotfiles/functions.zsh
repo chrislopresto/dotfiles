55ba916dea4f7cab33508e6dbfc460ad3c3a9b6a
Chris LoPresto <chris.lopresto@betterment.com>
2018-11-12 22:59:06 -0500

    Commence the culling

--- zsh/dotfiles/functions.zsh
+++ zsh/dotfiles/functions.zsh
@@ -9,21 +9,3 @@ function watchman_reset {
 function app_on_port {
   lsof -n -i4TCP:$1 | grep LISTEN
 }
-
-function df {
-  cd ~/.ellipsis/packages/dotfiles && stpf
-}
-
-function stpf {
-  if test -z "$(find . -maxdepth 1 -name '*.sublime-project' -print -quit)"; then
-    DIRECTORY_NAME=${PWD##*/}
-    cp ~/.ellipsis/packages/dotfiles/sublime/example.sublime-project "$DIRECTORY_NAME.sublime-project"
-  fi
-  st *.sublime-project
-}
-
-function t() {
-  # Defaults to 3 levels deep, do more with `t 5` or `t 1`
-  # pass additional args after
-  tree -I '.git|node_modules|bower_components|.DS_Store' --dirsfirst --filelimit 150 -L ${1:-3} -aC $2
-}
