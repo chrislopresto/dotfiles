c91010d6193aa040443bfc03fce6ef32ea0ef8ae
Chris LoPresto <chris@chrislopresto.com>
2026-03-09 21:27:14 -0400

    Add nvim commands to open current file in Code|Sublime

--- config/nvim/lua/config/keymaps.lua
+++ config/nvim/lua/config/keymaps.lua
@@ -1,3 +1,23 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Open current file in VS Code
+vim.api.nvim_create_user_command("OpenInCode", function()
+  local file = vim.fn.expand("%:p")
+  local line = vim.fn.line(".")
+  local col = vim.fn.col(".")
+  vim.fn.system({ "code", "--goto", file .. ":" .. line .. ":" .. col })
+end, { desc = "Open current file in VS Code" })
+
+vim.keymap.set("n", "<leader>oic", "<cmd>OpenInCode<cr>", { desc = "Open in Code" })
+
+-- Open current file in Sublime Text
+vim.api.nvim_create_user_command("OpenInSublime", function()
+  local file = vim.fn.expand("%:p")
+  local line = vim.fn.line(".")
+  local col = vim.fn.col(".")
+  vim.fn.system({ "subl", file .. ":" .. line .. ":" .. col })
+end, { desc = "Open current file in Sublime Text" })
+
+vim.keymap.set("n", "<leader>ois", "<cmd>OpenInSublime<cr>", { desc = "Open in Sublime" })
