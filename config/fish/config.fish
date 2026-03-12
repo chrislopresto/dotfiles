1e0eae96f9159a3311a4324dede2fd6c060f6770
Chris LoPresto <chris@chrislopresto.com>
2026-03-12 11:02:27 -0400

    Further simplify fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,12 +1,16 @@
-/opt/homebrew/bin/brew shellenv | source
+# Only run in interactive shells
+status is-interactive; or return
+
 set -g fish_greeting "🐟"
 
-if status is-interactive
-    if command --query starship
-        starship init fish | source
-    end
+if test -f /opt/homebrew/bin/brew
+    /opt/homebrew/bin/brew shellenv | source
+end
 
-    if command --query zoxide
-        zoxide init --cmd z fish | source
-    end
+if command --query starship
+    starship init fish | source
+end
+
+if command --query zoxide
+    zoxide init --cmd z fish | source
 end
