4182af467ec9244df55b5ff39fc82673f79cb609
Chris LoPresto <chris@chrislopresto.com>
2026-03-24 11:45:54 -0400

    Let snacks at hidden files

---
+++ config/nvim/lua/plugins/snacks.nvim.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "snacks.nvim",
+    opts = {
+      explorer = {
+        hidden = true,
+      },
+      picker = {
+        sources = {
+          files = {
+            hidden = true,
+          },
+          grep = {
+            hidden = true,
+          },
+        },
+      },
+    },
+  },
+}
